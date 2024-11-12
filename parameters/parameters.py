c0848984992d85a0d8a0e403cb8214458db512d5
orzi7 <140631583+orzi7@users.noreply.github.com>
2024-11-12 18:44:28 +0200

    Create parameters.py

---
+++ parameters/parameters.py
@@ -0,0 +1,19 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor
+from pybricks.parameters import Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+
+hub = PrimeHub()
+
+left_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE)
+right_motor = Motor(Port.A)
+
+WHEEL_DIAMETER = 62.4
+AXLE_TRACK = 124
+drive_base = DriveBase(left_motor, right_motor, WHEEL_DIAMETER, AXLE_TRACK)
+
+left_arm_motor = Motor(Port.D, Direction.COUNTERCLOCKWISE)
+right_arm_motor = Motor(Port.C)
+
+right_color_sensor = ColorSensor(Port.E)
+#left_color_sensor = ColorSensor(Port.F)
