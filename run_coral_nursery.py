acacef7e275e076a8de9305fc88de0894d4fc296
noaalfia <noaalfia2010@gmail.com>
2024-11-18 22:09:23 +0200

    added lines

--- run_coral_nursery.py
+++ run_coral_nursery.py
@@ -4,15 +4,33 @@ from pybricks.tools import wait
 from pid_drive import drive
 from pid_turn import turn
 
-drive(2, 0.003, 1, 0, 350, 15)
-turn(90, 7, 0.001, 0)
-drive(3, 0.003, 1, 0, 350, 5)
-turn(-90, 7, 0.001, 0)
-drive(2, 0.003, 1, 0, 350, 30)
-turn(-90, 7, 0.003, 0)
-left_arm_motor.run_angle(3000, 750, Stop.BRAKE, True)
+drive(2, 0.003, 1, 0, 350, 17)
+turn(-90, 7, 0.009, 0)
+drive(3, 0.003, 1, 0, -350, 3)
+drive(1, 0, 1, 0, 350, 53)
+turn(-90, 7, 0, 0)
+left_arm_motor.run_angle(3000, 790, Stop.BRAKE, True)
+drive(1.5, 0.003, 1, 0, 350, 13)
+left_arm_motor.run_angle(3000, -900, Stop.BRAKE, True)
+right_arm_motor.run_angle(6500, -1700, Stop.BRAKE, True)
+left_arm_motor.run_angle(3000, -250, Stop.BRAKE, True)
+right_arm_motor.run_angle(6000, 1600, Stop.BRAKE, True)
+left_arm_motor.run_angle(3000, 1000, Stop.BRAKE, True)
+drive(1.5, 0, 0, 0, -350, 13)
+left_arm_motor.run_angle(3000, -600, Stop.BRAKE, True)
+turn(70, 7, 0, 0)
+drive(1.5, 0, 0, 0, 350, 7)
+left_arm_motor.run_angle(9000, 1000, Stop.BRAKE, True)
+turn(40, 7, 0, 0)
+drive(1.5, 0, 0, 0, -350, 2)
+left_arm_motor.run_angle(6000, -1000, Stop.BRAKE, True)
+drive(1.5, 0, 0, 0, -350, 60)
 
 
-#right_arm_motor.run_angle(5000, -1200 , Stop.BRAKE, True)
-#right_arm_motor.run_angle(6000, 1200 , Stop.BRAKE, True)
+
+
+
+
+
+
 
