735dd92b07ff9366aabb6b8fdebff4b602376870
noaalfia <noaalfia2010@gmail.com>
2024-11-19 19:29:28 +0200

    ADD run_coral_nursery.py

--- run_coral_nursery.py
+++ run_coral_nursery.py
@@ -5,10 +5,10 @@ from pid_drive import drive
 from pid_turn import turn
 
 drive(2, 0.003, 1, 0, 350, 17)
-turn(-90, 7, 0.009, 0)
+turn(-90, 8.6, 0.009, 2.5)
 drive(3, 0.003, 1, 0, -350, 3)
 drive(1, 0, 1, 0, 350, 53)
-turn(-90, 7, 0, 0)
+turn(-90, 9, 0.006, 2.5)
 left_arm_motor.run_angle(3000, 790, Stop.BRAKE, True)
 drive(1.5, 0.003, 1, 0, 350, 13)
 left_arm_motor.run_angle(3000, -900, Stop.BRAKE, True)
@@ -18,19 +18,14 @@ right_arm_motor.run_angle(6000, 1600, Stop.BRAKE, True)
 left_arm_motor.run_angle(3000, 1000, Stop.BRAKE, True)
 drive(1.5, 0, 0, 0, -350, 13)
 left_arm_motor.run_angle(3000, -600, Stop.BRAKE, True)
-turn(70, 7, 0, 0)
+turn(70, 7, 0.007, 2)
 drive(1.5, 0, 0, 0, 350, 7)
-left_arm_motor.run_angle(9000, 1000, Stop.BRAKE, True)
-turn(40, 7, 0, 0)
+left_arm_motor.run_angle(10000, 1100, Stop.BRAKE, True)
+turn(40, 7, 0.007, 2)
 drive(1.5, 0, 0, 0, -350, 2)
+right_arm_motor.run_angle(6000, -900, Stop.BRAKE, True)
+drive(1.5, 0, 0, 0, 350, 4)
+right_arm_motor.run_angle(6000, -400, Stop.BRAKE, True)
+right_arm_motor.run_angle(6000, 1000, Stop.BRAKE, True)
 left_arm_motor.run_angle(6000, -1000, Stop.BRAKE, True)
-drive(1.5, 0, 0, 0, -350, 60)
-
-
-
-
-
-
-
-
-
+drive(1.5, 0, 0, 0, -350, 65)
