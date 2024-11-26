ecfa77ec41cbc347d91d64110aa8787a062d64b2
noaalfia <noaalfia2010@gmail.com>
2024-11-26 22:15:06 +0200

    Update run1_drivebase.py

--- run1_drivebase.py
+++ run1_drivebase.py
@@ -2,7 +2,7 @@ from parameters import left_arm_motor, right_arm_motor, hub, drive_base,
 from pybricks.parameters import Stop
 from pybricks.tools import wait
 
-drive_base.settings(300)
+drive_base.settings(300, turn_acceleration=275)
 drive_base.straight(140, Stop.BRAKE, True)
 drive_base.turn(-30, Stop.BRAKE, True)
 drive_base.straight(120, Stop.BRAKE, True)
@@ -47,38 +47,12 @@ drive_base.straight(230, Stop.BRAKE, True)
 drive_base.turn(-90, Stop.BRAKE, False)
 right_arm_motor.run_angle(2000, -1150, Stop.BRAKE, True)
 drive_base.straight(350, Stop.BRAKE, True)
-drive_base.turn(15, Stop.BRAKE, True)
-drive_base.straight(120, Stop.BRAKE, True)
-right_arm_motor.run_angle(2000, -600, Stop.BRAKE, True)
+drive_base.turn(20, Stop.BRAKE, True)
+drive_base.straight(140, Stop.BRAKE, True)
+
+#Click on the yellow
+right_arm_motor.run_angle(2000, -800, Stop.BRAKE, True)
 right_arm_motor.run_angle(2000, 750, Stop.BRAKE, True)
 drive_base.straight(-200, Stop.BRAKE, True)
 drive_base.turn(20, Stop.BRAKE, False)
 drive_base.straight(-650, Stop.BRAKE, True)
-
-
-
-
-'''
-right_arm_motor.run_angle(2000, -1200, Stop.BRAKE, True)
-drive_base.settings(350)
-drive_base.straight(200, Stop.BRAKE, True)
-right_arm_motor.run_angle(2000, -500, Stop.BRAKE, True)
-
-
-right_arm_motor.run_angle(2000, 300, Stop.BRAKE, True)
-drive_base.straight(-20, Stop.BRAKE, True)
-right_arm_motor.run_angle(2000, 1000, Stop.BRAKE, True)
-drive_base.straight(-550, Stop.BRAKE, True)
-
-drive_base.settings(500)
-
-drive_base.straight(700, Stop.BRAKE, True)
-drive_base.settings(300)
-drive_base.straight(200, Stop.BRAKE, True)
-
-drive_base.settings(450)
-drive_base.straight(550, Stop.BRAKE, True)
-wait(200)
-drive_base.straight(-200, Stop.BRAKE, True)
-
-'''
