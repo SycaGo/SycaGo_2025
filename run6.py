54e3d9171d7cd8588836a8d7400506fd1e3fd6db
noaalfia <noaalfia2010@gmail.com>
2024-12-01 21:16:41 +0200

    Create run6.py

---
+++ run6.py
@@ -0,0 +1,28 @@
+from parameters import left_arm_motor, right_arm_motor,hub,drive_base
+from pybricks.parameters import Stop
+from pybricks.tools import wait
+from turn_general import turn_general
+from drive_general import drive_general
+
+drive_general(170, 350, True)
+
+turn_general(-30, 275, True)
+
+drive_general(160, 350, True)
+
+turn_general(38 ,275, True)
+
+drive_general(160, 350, True)
+
+turn_general(-30, 275, True)
+
+drive_general(200, 350, True)
+
+drive_general(-170, 350, True)
+
+turn_general(36, 275, True)
+
+drive_general(240, 350, True)
+
+drive_general(-150, 350, True)
+
