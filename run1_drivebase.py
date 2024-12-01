2c71741a435950b6fa337174d840b91cc21e4026
noaalfia <noaalfia2010@gmail.com>
2024-12-01 21:16:37 +0200

    change to drive and turn general

--- run1_drivebase.py
+++ run1_drivebase.py
@@ -1,58 +1,60 @@
 from parameters import left_arm_motor, right_arm_motor, hub, drive_base,
 from pybricks.parameters import Stop
 from pybricks.tools import wait
+from drive_general import drive_general
+from turn_general import turn_general
 
-drive_base.settings(300, turn_acceleration=275)
-drive_base.straight(140, Stop.BRAKE, True)
-drive_base.turn(-30, Stop.BRAKE, True)
-drive_base.straight(120, Stop.BRAKE, True)
-drive_base.turn(-60, Stop.BRAKE, True)
+drive_general(140, 350, True)
+turn_general(-25, 270, True)
+drive_general(120, 350, True)
+turn_general(-60, 270, True)
 
 #backward to the wall
-drive_base.settings(250)
-drive_base.straight(-90, Stop.BRAKE, True)
+drive_general(-90, 250, True)
 
 #drive forward to the mission
-drive_base.settings(400)
-drive_base.straight(640, Stop.BRAKE, True)
-drive_base.turn(-90, Stop.BRAKE, True)
+drive_general(640, 400, True)
+turn_general(-90, 200, True)
 left_arm_motor.run_angle(3000, 860, Stop.BRAKE, True)
-drive_base.straight(140, Stop.BRAKE, True)
+drive_general(140, 330, True)
 left_arm_motor.run_angle(2000, -1200, Stop.BRAKE, False)
 right_arm_motor.run_angle(3500, -2000, Stop.BRAKE, True)
 
 #return the arms
 right_arm_motor.run_angle(2000, 1600, Stop.BRAKE, True)
 left_arm_motor.run_angle(3000, 1000, Stop.BRAKE, True)
-drive_base.settings(300)
-drive_base.straight(-140, Stop.BRAKE, True)
+drive_general(-140, 300, True)
 left_arm_motor.run_angle(3000, -600, Stop.BRAKE, True)
-drive_base.turn(75, Stop.BRAKE, True)
-drive_base.straight(40, Stop.BRAKE, True)
+turn_general(70, 350, True)
+drive_general(80, 300, True)
 
 #drop the shark
 left_arm_motor.run_angle(10000, 1250, Stop.BRAKE, True)
-drive_base.straight(-60, Stop.BRAKE, True)
+drive_general(-60, 350, True)
 left_arm_motor.run_angle(10000, -1250, Stop.BRAKE, True)
 
 #backward to the wall
-drive_base.turn(37, Stop.BRAKE, True)
-drive_base.straight(-700, Stop.BRAKE, True)
-wait(3000)
+turn_general(30, 300, True)
+drive_general(-700, 350, False)
+right_arm_motor.run_angle(2000, 300, Stop.BRAKE, True)
+right_arm_motor.reset_angle()
+
+
+wait(2000)
 
 #go back to mission 3
-drive_base.straight(250, Stop.BRAKE, True)
-drive_base.turn(90, Stop.BRAKE, True)
-drive_base.straight(230, Stop.BRAKE, True)
-drive_base.turn(-90, Stop.BRAKE, False)
+drive_general(250, 300, True)
+turn_general(90, 250, True)
+drive_general(200, 300, True)
+turn_general(-90, 250, True)
 right_arm_motor.run_angle(2000, -1150, Stop.BRAKE, True)
-drive_base.straight(350, Stop.BRAKE, True)
-drive_base.turn(20, Stop.BRAKE, True)
-drive_base.straight(140, Stop.BRAKE, True)
+drive_general(350, 350, True)
+turn_general(20, 250, True)
+drive_general(250, 400, True)
 
 #Click on the yellow
-right_arm_motor.run_angle(2000, -800, Stop.BRAKE, True)
-right_arm_motor.run_angle(2000, 750, Stop.BRAKE, True)
-drive_base.straight(-200, Stop.BRAKE, True)
-drive_base.turn(20, Stop.BRAKE, False)
-drive_base.straight(-650, Stop.BRAKE, True)
+right_arm_motor.run_angle(2000, -1050, Stop.BRAKE, True)
+right_arm_motor.run_angle(2000, 800, Stop.BRAKE, True)
+drive_general(-200, 350, True)
+turn_general(10, 350,True)
+drive_general(-400, 400, True)
