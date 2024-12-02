deb88777d2d086f94523126e0f612e503a8ac722
noaalfia <noaalfia2010@gmail.com>
2024-12-02 21:56:49 +0200

    Update run1_drivebase.py

--- run1_drivebase.py
+++ run1_drivebase.py
@@ -7,7 +7,7 @@ from turn_general import turn_general
 drive_general(140, 350, True)
 turn_general(-25, 270, True)
 drive_general(120, 350, True)
-turn_general(-60, 270, True)
+turn_general(-65, 270, True)
 
 #backward to the wall
 drive_general(-90, 250, True)
@@ -17,8 +17,8 @@ drive_general(640, 400, True)
 turn_general(-90, 200, True)
 left_arm_motor.run_angle(3000, 860, Stop.BRAKE, True)
 drive_general(140, 330, True)
-left_arm_motor.run_angle(2000, -1200, Stop.BRAKE, False)
-right_arm_motor.run_angle(3500, -2000, Stop.BRAKE, True)
+left_arm_motor.run_angle(1800, -1200, Stop.BRAKE, False)
+right_arm_motor.run_angle(3000, -2100, Stop.BRAKE, True)
 
 #return the arms
 right_arm_motor.run_angle(2000, 1600, Stop.BRAKE, True)
@@ -44,17 +44,16 @@ wait(2000)
 
 #go back to mission 3
 drive_general(250, 300, True)
-turn_general(90, 250, True)
+turn_general(90, 100, True)
 drive_general(200, 300, True)
-turn_general(-90, 250, True)
-right_arm_motor.run_angle(2000, -1150, Stop.BRAKE, True)
+turn_general(-90, 100, True)
+right_arm_motor.run_angle(2000, -1150, Stop.HOLD, True)
 drive_general(350, 350, True)
 turn_general(20, 250, True)
-drive_general(250, 400, True)
+drive_general(250, 350, True)
 
 #Click on the yellow
 right_arm_motor.run_angle(2000, -1050, Stop.BRAKE, True)
 right_arm_motor.run_angle(2000, 800, Stop.BRAKE, True)
 drive_general(-200, 350, True)
-turn_general(10, 350,True)
-drive_general(-400, 400, True)
+drive_general(-550, 400, True)
