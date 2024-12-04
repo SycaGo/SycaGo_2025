0176ac44cad185a0b54f9a39a6e6fe2e81230450
noaalfia <noaalfia2010@gmail.com>
2024-12-04 22:16:39 +0200

    Update run6.py

--- run6.py
+++ run6.py
@@ -4,32 +4,21 @@ from pybricks.tools import wait
 from turn_general import turn_general
 from drive_general import drive_general
 
+#take Krill 1
 drive_general(170, 350, True)
+turn_general(-50, 275, True)
+drive_general(320, 350, True)
+drive_general(-90, 350, True)
 
-turn_general(-50, 250, True)
+#take green Reef Segments
+turn_general(45 ,275, True)
+drive_general(160, 350, True)
 
-drive_general(260, 350, True)
-
-turn_general(-30, 250, True)
-
-drive_general(150, 200, True)
-
-drive_general(-200, 350, True)
-
-turn_general(70, 250, True)
-
-drive_general(150, 350, True)
-
-turn_general(-22, 250, True)
-
-drive_general(230, 350, True)
-
-drive_general(-85, 350, True)
-
-turn_general(65, 250, True)
-
+# take Krill 2
+turn_general(-15, 275, True)
 drive_general(200, 350, True)
-
-drive_general(-150, 350, True)
-
-turn_general(-35, 250, True)
+turn_general(15, 275, True)
+drive_general(250, 350, True)
+drive_general(-50, 350, True)
+turn_general(30, 275, True)
+drive_general(-200, 350, True)
