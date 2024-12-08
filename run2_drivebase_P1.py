c753d695a5b3f88918449bcd237864a4df78b0fc
noaalfia <noaalfia2010@gmail.com>
2024-12-08 17:29:55 +0200

    separate run 2 to two parts

--- run2_drivebase_P1.py
+++ run2_drivebase_P1.py
@@ -40,22 +40,3 @@ turn_general(30, 300, True)
 drive_general(-700, 350, False)
 right_arm_motor.run_angle(2000, 300, Stop.BRAKE, True)
 right_arm_motor.reset_angle()
-
-wait(2500)
-
-#go back to mission 3
-drive_general(250, 300, True)
-turn_general(90, 100, True)
-drive_general(200, 300, True)
-turn_general(-90, 100, True)
-right_arm_motor.run_angle(2000, -1150, Stop.HOLD, True)
-drive_general(350, 350, True)
-turn_general(20, 250, True)
-drive_general(250, 350, True)
-
-#Click on the yellow
-right_arm_motor.run_angle(2000, -1050, Stop.BRAKE, True)
-right_arm_motor.run_angle(2000, 800, Stop.BRAKE, True)
-drive_general(-200, 350, True)
-turn_general(-15, 350, True)
-drive_general(-550, 400, True)
