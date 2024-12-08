2b39c91daae58987fb9ad36b3f314eaf0fd261ac
noaalfia <noaalfia2010@gmail.com>
2024-12-08 19:20:01 +0200

    Update run1_take.py
    changed the numbers and countinude to other mission (14)

--- run1_take.py
+++ run1_take.py
@@ -3,25 +3,45 @@ from pybricks.parameters import Stop
 from pybricks.tools import wait
 from turn_general import turn_general
 from drive_general import drive_general
-
+'''
 drive_general(150, 300, True)
 
-turn_general(32, 275, True)
+turn_general(30, 275, True)
 
 drive_general(230, 300, True)
 
-turn_general(-50, 275, True)
+turn_general(-35, 275, True)
 
 drive_general(150, 300, True)
 
 turn_general(25, 275, True)
 
+hub.imu.reset_heading(0)
+
 drive_general(170, 300, True)
 
-drive_general(-270, 300, True)
+drive_general(-300, 300, True)
 
-turn_general(25, 275, True)
+turn_general(19, 275, True)
 
-drive_general(370, 300, True)
+drive_general(345, 300, True)
 
-drive_general(-800, 300, True)
+turn_general(35, 275, True)
+
+hub.imu.reset_heading(0)
+
+drive_general(470, 300, True)
+
+
+
+
+turn_general(-60, 275, True)
+
+drive_general(5, 300, True)
+
+#drive_general(-800, 300, True)
+'''
+
+drive_general(150, 200, True)
+drive_general(190, 100, True)
+drive_general(-300, 150, True)
