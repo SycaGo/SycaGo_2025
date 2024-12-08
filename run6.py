997231916c73e5de12d2fee9add7ec7a0a15c7d5
noaalfia <noaalfia2010@gmail.com>
2024-12-08 19:26:43 +0200

    Update run6.py

--- run6.py
+++ run6.py
@@ -6,19 +6,17 @@ from drive_general import drive_general
 
 #take Krill 1
 drive_general(170, 350, True)
-turn_general(-50, 275, True)
-drive_general(320, 350, True)
-drive_general(-90, 350, True)
+turn_general(-42, 150, True)
+drive_general(280, 350, True)
+drive_general(-110, 350, True)
 
 #take green Reef Segments
-turn_general(45 ,275, True)
-drive_general(160, 350, True)
+turn_general(35, 150, True)
+drive_general(220, 350, True)
+drive_general(-70, 350, True)
 
 # take Krill 2
-turn_general(-15, 275, True)
+turn_general(-20, 150, True)
 drive_general(200, 350, True)
-turn_general(15, 275, True)
-drive_general(250, 350, True)
-drive_general(-50, 350, True)
-turn_general(30, 275, True)
-drive_general(-200, 350, True)
+turn_general(14, 150, True)
+drive_general(200, 350, True)
