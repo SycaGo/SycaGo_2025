9b828e28143ff661f276715f27414f9134b8b1e6
noaalfia <noaalfia2010@gmail.com>
2024-12-12 17:54:44 +0200

    Update run6.py

--- run6.py
+++ run6.py
@@ -4,12 +4,12 @@ from pybricks.tools import wait
 from turn_general import turn_general
 from drive_general import drive_general
 
-right_arm_motor.run_angle(2000, -250, Stop.BRAKE, True)
+drive_general(10, 450, True)
 
 #take Krill 1
 drive_general(170, 450, True)
 turn_general(-42, 150, True)
-drive_general(280, 450, True)
+drive_general(300, 450, True)
 drive_general(-110, 450, True)
 
 #take green Reef Segments
@@ -26,3 +26,15 @@ turn_general(20, 150, True)
 drive_general(300, 450, True)
 turn_general(40, 150, True)
 drive_general(-200, 450, True)
+
+# take or not Krill 3
+hub.imu.reset_heading(0)
+turn_general(35, 150, True)
+drive_general(220, 600, True)
+drive_general(-200, 450, True)
+
+# take Plankton Sample
+turn_general(20, 150, True)
+drive_general(220, 200, True)
+drive_general(-220, 450, True)
+drive_general(150, 450, True)
