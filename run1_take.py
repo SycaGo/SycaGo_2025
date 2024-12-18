846236cbd0ddd68479a273e76acc85d8b735710c
noaalfia <noaalfia2010@gmail.com>
2024-12-18 21:39:53 +0200

    Update run1_take.py

--- run1_take.py
+++ run1_take.py
@@ -3,45 +3,39 @@ from pybricks.parameters import Stop
 from pybricks.tools import wait
 from turn_general import turn_general
 from drive_general import drive_general
-'''
-drive_general(150, 300, True)
+
+drive_general(150, 350, True)
+
+turn_general(40, 350, True)
+
+drive_general(270, 350, True)
+
+turn_general(-46, 275, True)
+
+drive_general(130, 300, True)
+
+turn_general(22, 100, True)
+
+drive_general(190, 300, True)
+
+turn_general(-20, 100, True)
+
+drive_general(280, 350, True)
+
+drive_general(-450, 300, True)
+
+turn_general(24, 275, True)
+
+drive_general(290, 300, True)
+
+turn_general(60, 275, True)
+
+drive_general(300, 300, True)
 
 turn_general(30, 275, True)
 
-drive_general(230, 300, True)
+drive_general(200, 300, True)
 
-turn_general(-35, 275, True)
+turn_general(-30, 275, True)
 
-drive_general(150, 300, True)
-
-turn_general(25, 275, True)
-
-hub.imu.reset_heading(0)
-
-drive_general(170, 300, True)
-
-drive_general(-300, 300, True)
-
-turn_general(19, 275, True)
-
-drive_general(345, 300, True)
-
-turn_general(35, 275, True)
-
-hub.imu.reset_heading(0)
-
-drive_general(470, 300, True)
-
-
-
-
-turn_general(-60, 275, True)
-
-drive_general(5, 300, True)
-
-#drive_general(-800, 300, True)
-'''
-
-drive_general(150, 200, True)
-drive_general(190, 100, True)
-drive_general(-300, 150, True)
+#left_arm_motor.run_angle(300, -50, Stop.BRAKE, True)
