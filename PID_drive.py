6c13d580972a4a475b0f1728869809424bec26a4
noaalfia <noaalfia2010@gmail.com>
2024-12-25 17:55:56 +0200

    Revert "Update PID_drive.py"
    This reverts commit 7e6391bc5b57480387feaec2abf961ce037d63eb.

--- PID_drive.py
+++ PID_drive.py
@@ -1,25 +1,21 @@
-from parameters import left_motor, right_motor, hub, drive_base, WHEEL_DIAMETER
-from pybricks.parameters import Stop
+from parameters import drive_base, hub, right_motor, left_motor, WHEEL_DIAMETER
 from pybricks.tools import wait
-
 
 def calculate_distance():
     right_motor_angle = right_motor.angle()
     left_motor_angle = left_motor.angle()
     P = WHEEL_DIAMETER * 3.14192
     average_angle = (abs(right_motor_angle) + abs(left_motor_angle)) / 2
-    distance_driven = ((average_angle / 360) * P)
+    distance_driven = ((average_angle / 360) * P) / 10
     return distance_driven
 
-
-def drive_general(distance, speed):
-    set_point = 0
-    kp = 1.5
+def drive(kp, ki, kd, set_point, speed, distance):
     integral = 0
     last_error = 0
     left_motor.reset_angle(0)
     right_motor.reset_angle(0)
     hub.imu.reset_heading(0)
+
     scale = abs(set_point / 10)
     distance_passed = calculate_distance()
     
@@ -31,14 +27,25 @@ def drive_general(distance, speed):
         distance_passed = calculate_distance()
         error = set_point - hub.imu.heading()
         p = error * kp
-        correction = p
+        d = (error - last_error) * kd
+        correction = p + d
+        
+        if abs(hub.imu.heading()) >= (abs(set_point) - scale):
+            integral += error
+            i = integral * ki
+            correction = p + i + d
+            
+        last_error = error
+
         if distance_passed < deceleration_starting_position:
             speed = max_speed
         else:
             speed = (final_speed - max_speed) * ((distance_passed - deceleration_starting_position) / deceleration_range) + max_speed
         
         drive_base.drive(speed, correction)
+
+
     left_motor.brake()
     right_motor.brake()
-    wait(100)
 
+    wait(100)
