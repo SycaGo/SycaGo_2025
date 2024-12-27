2417b3353a34fe4a0b4b789c7cde86551baf7a77
noaalfia <noaalfia2010@gmail.com>
2024-12-27 18:51:41 +0200

    adjustment to changes in the drive general and turn general

--- run_3.py
+++ run_3.py
@@ -4,18 +4,15 @@ from pybricks.tools import wait
 from drive_general import drive_general
 from turn_general import turn_general
 
-drive_general(-20, 300, True)
-drive_general(350, 300, True)
-turn_general(30, 250, True)
-drive_general(180, 350, True)
-turn_general(60, 130, True)
-drive_general(200, 300, True)
+drive_general(350, 300)
+turn_general(30)
+drive_general(180, 350)
+turn_general(60)
+drive_general(200, 300)
 
 #drive forward to the mission
-drive_general(110, 50, True)
-drive_general(-300, 100, True)
+drive_general(110, 70)
+drive_general(300, -100)
 
 #get the treasure ches in the oneway
-drive_general(200, 500, True)
-
-
+drive_general(200, 500)
