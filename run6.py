e1487642f821b50b90d48dd2d97180716f05b60b
noaalfia <noaalfia2010@gmail.com>
2024-12-27 13:43:16 +0200

    Update run6.py

--- run6.py
+++ run6.py
@@ -4,19 +4,18 @@ from pybricks.tools import wait
 from turn_general import turn_general
 from drive_general import drive_general
 
-
 #take Krill 1
 drive_general(160, 350)
-turn_general(-45, 200, True)
+turn_general(-45)
 drive_general(240, 350)
 drive_general(70, -350)
 
 # take Krill 2
-turn_general(35, 200, True)
+turn_general(35)
 drive_general(180, 350)
 
 #take green Reef Segments
-turn_general(-20, 200, True)
+turn_general(-20)
 drive_general(150, 350)
 
 # do the Sonar Discovery
