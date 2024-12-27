f67a6dfdba108105b381f337b00027ca0fb15804
noaalfia <noaalfia2010@gmail.com>
2024-12-27 12:02:19 +0200

    Update turn_general.py

--- turn_general.py
+++ turn_general.py
@@ -2,6 +2,6 @@ from parameters import left_arm_motor, right_arm_motor, hub, drive_base
 from pybricks.parameters import Stop
 from pybricks.tools import wait
 
-def turn_general(turn_degrees, speed, bool):
+def turn_general(turn_degrees, speed, dsync):
     drive_base.settings(turn_rate=speed)
-    drive_base.turn(turn_degrees, Stop.BRAKE, bool)
+    drive_base.turn(turn_degrees, Stop.BRAKE, dsync)
