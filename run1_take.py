801f1018940d956d458d68af2f46f6d3ecf98ec9
noaalfia <noaalfia2010@gmail.com>
2025-01-01 22:04:54 +0200

    Update run1_take.py

--- run1_take.py
+++ run1_take.py
@@ -5,45 +5,53 @@ from turn_general import turn_general
 from drive_general import drive_general
 
 #take first coral
-drive_general(150, 350)
+drive_general(130, 400)
 
 turn_general(40)
 
-drive_general(220, 350)
+drive_general(280, 400)
+drive_general(50, -400)
 
 #take second coral
-turn_general(-48)
+turn_general(-40)
 
-drive_general(130, 300)
-
+drive_general(220, 400)
+drive_general(50, -400)
 #take first shrimp
-turn_general(18)
+turn_general(17)
 
-drive_general(170, 300)
+drive_general(190, 350)
 
 #Alignment on a wall
-turn_general(-12)
+turn_general(-17)
 
-drive_general(300, 350)
+drive_general(300, 330)
 
 #take first water sample
-drive_general(450, -300)
+drive_general(450, -500)
 
 turn_general(20)
 
-drive_general(280, 300)
+drive_general(250, 350)
 
 #take second water sample
-turn_general(30)
+turn_general(28)
 
-drive_general(200, 300)
+drive_general(200, 350)
 
 turn_general(40)
 
-drive_general(280, 300)
+drive_general(240, 350)
 
-turn_general(-30)
+turn_general(-20)
 
-drive_general(50, 300)
+drive_general(65, 300)
 
-right_arm_motor.run_angle(15000, 2000, Stop.BRAKE, True )
+left_arm_motor.run_angle(300, -500, Stop.BRAKE, True )
+
+#go back home
+drive_general(150, -600)
+turn_general(20)
+drive_general(200, -600)
+turn_general(70)
+drive_general(400, 600)
