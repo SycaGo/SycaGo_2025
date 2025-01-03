85ca38bc98fd3daf1fecc40b2c3edef72cf9b1fd
noaalfia <noaalfia2010@gmail.com>
2025-01-03 16:56:08 +0200

    Update run7.py
    continued mission 9

--- run7.py
+++ run7.py
@@ -3,13 +3,20 @@ from pybricks.parameters import Stop
 from pybricks.tools import wait
 from turn_general import turn_general
 from drive_general import drive_general
-from line_alligment import line_alignment
+from line_aligment import line_alignment
 
 #mission 9
 drive_general(130, 400)
-turn_general(-30)
+turn_general(-27)
 drive_general(400,400)
 turn_general(-20)
 drive_general(150,400)
-turn_general(-15)
-line_alignment(350)
+turn_general(-20)
+line_alignment(500)
+turn_general(-60)
+right_arm_motor.run_angle(-10000, 1500, Stop.BRAKE, True)
+turn_general(-10)
+drive_general(80, 400)
+turn_general(10)
+
+
