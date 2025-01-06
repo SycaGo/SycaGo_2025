c8b445007e1fea97628a15af4855132fef8f6777
noaalfia <noaalfia2010@gmail.com>
2025-01-06 18:31:24 +0200

    Update run_3.py

--- run_3.py
+++ run_3.py
@@ -6,31 +6,24 @@ from turn_general import turn_general
 
 drive_general(310, 400)
 turn_general(30)
-drive_general(170, 350)
+drive_general(160, 350)
 turn_general(60)
 
 #drive forward to the mission
-drive_general(100, 250)
+drive_general(110, 150)
 drive_general(100, 70)
-right_arm_motor.run_angle(100, 300, Stop.BRAKE, True)
-drive_general(180, -300)
+right_arm_motor.run_angle(200, 300, Stop.BRAKE, True)
+drive_general(140, -200)
 
 #put the srimp in the house
 turn_general(100)
-right_arm_motor.run_angle(10000, -300, Stop.BRAKE, True)
-turn_general(-10)
+drive_general(40, 300)
 
-#Alignment on a wall
-drive_general(400, -350)
-drive_general(100, 350)
-turn_general(-83)
-drive_general(140, 350)
+right_arm_motor.run_angle(10000, -300, Stop.BRAKE, True)
 turn_general(-15)
 
-#put the scubadiver on the yellow line
+#Alignment on a wall
+drive_general(450, -350)
 left_arm_motor.run_angle(5000, 900, Stop.BRAKE, True)
-left_arm_motor.run_angle(5000, -600, Stop.BRAKE, True)
-turn_general(-35)
-drive_general(140, -350)
-
-
+left_arm_motor.run_angle(5000, -200, Stop.BRAKE, True)
+drive_general(800, 900)
