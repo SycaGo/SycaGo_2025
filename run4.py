816ba373d911474069c13b063236c06b76993e82
noaalfia <noaalfia2010@gmail.com>
2025-01-08 00:03:23 +0200

    Update run4.py

--- run4.py
+++ run4.py
@@ -1,12 +1,23 @@
-from parameters import left_arm_motor, right_arm_motor, hub, drive_base
+from parameters import left_arm_motor, right_arm_motor,hub,drive_base
 from pybricks.parameters import Stop
 from pybricks.tools import wait
-from drive_general import drive_general
 from turn_general import turn_general
-drive_general(400, 400)
-turn_general(40)
-drive_general(230, 450)
-turn_general(-38)
-drive_general(300, 450)
-right_arm_motor.run_angle(300, 280, Stop.BRAKE, True)
-drive_general(200, -100)
+from drive_general import drive_general
+drive_general(220, 400)
+turn_general(30)
+drive_general(350, 400)
+turn_general(-30)
+drive_general(450,400)
+left_arm_motor.run_angle(400,270,Stop.BRAKE, True)
+drive_general(60,-30)
+left_arm_motor.run_angle(510,100,Stop.BRAKE, True)
+drive_general(150,-30)
+left_arm_motor.run_angle(500,-70,Stop.BRAKE, True)
+drive_general(50,400)
+left_arm_motor.run_angle(500,70,Stop.BRAKE, True)
+turn_general(50)
+drive_general(700,-700)
+turn_general(50)
+drive_general(50,700)
+
+
