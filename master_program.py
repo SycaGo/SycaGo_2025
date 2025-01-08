6a3d426d291adae01e12ea07ee6bc6720eb7d2e0
noaalfia <noaalfia2010@gmail.com>
2025-01-08 16:38:53 +0200

    Create master_program.py

---
+++ master_program.py
@@ -0,0 +1,20 @@
+from pybricks.tools import hub_menu
+
+selected = hub_menu("1", "2", "3", "4", "5", "6", "7", "8")
+
+if selected == "1":
+    import run1
+elif selected == "2":
+    import run2
+elif selected == "3":
+    import run3
+elif selected == "4":
+    import run4
+elif selected == "5":
+    import run5
+elif selected == "6":
+    import run6
+elif selected == "7":
+    import run7
+elif selected == "8":
+    import run8
