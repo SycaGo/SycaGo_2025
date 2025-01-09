8f241760a689f33a75ffed281e93ebce20dc86cf
noaalfia <noaalfia2010@gmail.com>
2025-01-09 17:19:04 +0200

    Update run7.py

--- run7.py
+++ run7.py
@@ -3,14 +3,15 @@ from pybricks.parameters import Stop
 from pybricks.tools import wait
 from turn_general import turn_general
 from drive_general import drive_general
-from line_alligment import line_alignment
 drive_general(200,400, 0, False)
 turn_general(-90)
-drive_general(500,400, -90, True)
+drive_general(530, 400, -90, True)
 turn_general(-95)
 drive_general(420,200, -95, True)
 drive_general(10,-300, -95, True)
-drive_general(100,200, -95, True)
+drive_general(100,400, -95, True)
 drive_general(80,-100, -95, True)
-turn_general(-95)
-drive_general(600,550, -95, True)
+turn_general(90)
+drive_general(250,-600, 90, True)
+turn_general(90)
+drive_general(420,600, 90, True)
