248256da23c1c7d258350b7b184ed5c0063ed6e2
noaalfia <noaalfia2010@gmail.com>
2025-01-09 23:25:59 +0200

    Update run7.py

--- run7.py
+++ run7.py
@@ -1,17 +1,26 @@
-from parameters import left_arm_motor, right_arm_motor,hub,drive_base
+from parameters import left_arm_motor, right_arm_motor,hub,drive_base, right_motor, left_motor
 from pybricks.parameters import Stop
 from pybricks.tools import wait
 from turn_general import turn_general
 from drive_general import drive_general
+
 drive_general(200,400, 0, False)
 turn_general(-90)
 drive_general(530, 400, -90, True)
 turn_general(-95)
-drive_general(420,200, -95, True)
-drive_general(10,-300, -95, True)
-drive_general(100,400, -95, True)
-drive_general(80,-100, -95, True)
+drive_general(200, 200, -95, True)
+drive_general(10, -300, -95, True)
+drive_general(30, 400, -95, True)
+drive_general(100, -100, -95, True)
 turn_general(90)
-drive_general(250,-600, 90, True)
+drive_general(200, -600, 90, True)
 turn_general(90)
-drive_general(420,600, 90, True)
+drive_general(390, -440, 90, True)
+drive_general(620, 450, 90, True)
+turn_general(-45)
+right_arm_motor.run_angle(10000, 1300, Stop.BRAKE, True)
+drive_general(90, -400, -45, True)
+right_arm_motor.run_angle(-10000, 1000, Stop.BRAKE, True)
+turn_general(45)
+drive_general(700, -800, 45, True)
+
