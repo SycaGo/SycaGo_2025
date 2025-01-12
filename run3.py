b01e4a1a8d48bc3c58b7bfcd09f4751aef5b3387
noaalfia <noaalfia2010@gmail.com>
2025-01-12 17:01:33 +0200

    Update run3.py

--- run3.py
+++ run3.py
@@ -6,25 +6,26 @@ from turn_general import turn_general
 
 drive_general(290, 500, 0, False)
 turn_general(30)
-drive_general(170, 300, 30, True)
+drive_general(170, 200, 30, True)
 turn_general(60)
 
 #drive forward to the mission
-drive_general(100, 300, 60, True)
-drive_general(110, 60, 60, True)
-right_arm_motor.run_angle(250, 320, Stop.BRAKE, True)
-drive_general(110, -200, 60, True)
+drive_general(100, 300, 0, False)
+drive_general(110, 60, 0, False)
+right_arm_motor.run_angle(100, 120, Stop.BRAKE, True)
+right_arm_motor.run_angle(500, 200, Stop.BRAKE, True)
+drive_general(110, -200, 0, False)
 
 #put the srimp in the house
 turn_general(100)
-drive_general(40, 300, 100, True)
+drive_general(60, 300, 100, True)
 
 right_arm_motor.run_angle(8000, -300, Stop.BRAKE, True)
 turn_general(-20)
 
 #Alignment on a wall
-drive_general(420, -550, -20, True)
-left_arm_motor.run_angle(10000, 900, Stop.BRAKE, True)
+drive_general(420, -600, -20, True)
+left_arm_motor.run_angle(10000, 1000, Stop.BRAKE, True)
 left_arm_motor.run_angle(5000, -250, Stop.BRAKE, True)
 drive_general(500, 550,-20, True)
 turn_general(50)
