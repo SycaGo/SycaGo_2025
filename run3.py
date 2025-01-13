9d6d580b3b6573a321946a196f03863fb45a8d72
noaalfia <noaalfia2010@gmail.com>
2025-01-13 22:46:41 +0200

    change the strategy

--- run3.py
+++ run3.py
@@ -3,7 +3,7 @@ from pybricks.parameters import Stop
 from pybricks.tools import wait
 from drive_general import drive_general
 from turn_general import turn_general
-
+'''
 drive_general(300, 500, 0, False)
 turn_general(30)
 drive_general(170, 330 , 30, True)
@@ -30,3 +30,17 @@ left_arm_motor.run_angle(10000, -250, Stop.BRAKE, True)
 drive_general(500, 1000,0, False)
 turn_general(60)
 drive_general(350, 1000, 60, True)
+'''
+
+drive_general(120, 350, 0, False)
+turn_general(90)
+drive_general(650, -500, 90, True)
+turn_general(15)
+drive_general(150, -600, 15, True)
+left_arm_motor.run_angle(10000, 1000, Stop.BRAKE, True)
+left_arm_motor.run_angle(10000, -350, Stop.BRAKE, True)
+drive_general(300, 600, 0, False)
+turn_general(-90)
+drive_general(230, 150, -90, True)
+right_arm_motor.run_angle(100, 120, Stop.BRAKE, True)
+right_arm_motor.run_angle(500, 200, Stop.BRAKE, True)
