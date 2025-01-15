67c9b9ef6d003a2c22bb805ee1f3abdf8fc26a25
noaalfia <noaalfia2010@gmail.com>
2025-01-15 22:45:16 +0200

    Update run1.py

--- run1.py
+++ run1.py
@@ -3,17 +3,19 @@ from pybricks.parameters import Stop
 from pybricks.tools import wait
 from turn_general import turn_general
 from drive_general import drive_general
-drive_general(600, 550, 0, False)
+
+drive_general(640, 400, 0, False)
 turn_general(22)
-drive_general(330, 450, 22, True)
+drive_general(315, 450, 22, True)
 wait(400)
 right_arm_motor.run_angle(300, 400, Stop.BRAKE, True)
 turn_general(120)
-left_arm_motor.run_angle(800, 250, Stop.BRAKE, True)
+drive_general(40, 200, 120, True)
+left_arm_motor.run_angle(600, 185, Stop.BRAKE, True)
 drive_general(100, -400, 120, True)
-left_arm_motor.run_angle(800, -250, Stop.BRAKE, True)
-drive_general(50, 200, 120, True)
+left_arm_motor.run_angle(300, -250, Stop.BRAKE, True)
+drive_general(50, 200, 0, False)
 turn_general(60)
 drive_general(250, 500, 60, True)
 turn_general(-25)
-drive_general(620, 800, -25, True)
+drive_general(630, 800, -25, True)
