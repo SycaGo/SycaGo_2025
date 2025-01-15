5d2dec09c69c5c22036ef5e042684ef983e07e23
noaalfia <noaalfia2010@gmail.com>
2025-01-15 22:44:39 +0200

    Update the final speed, the calibration

--- drive_general.py
+++ drive_general.py
@@ -6,15 +6,14 @@ from pybricks.tools import wait
 def calculate_distance():
     right_motor_angle = right_motor.angle()
     left_motor_angle = left_motor.angle()
-    P = WHEEL_DIAMETER * 3.14192
+    P = WHEEL_DIAMETER * 3.141592
     average_angle = (abs(right_motor_angle) + abs(left_motor_angle)) / 2
     distance_driven = ((average_angle / 360) * P)
     return distance_driven
 
-
 def drive_general(distance, speed, set_point, absolute=True):
-    kp = 1.2
-    kd = 0.2
+    kp = 0.7
+    kd = 2
     left_motor.reset_angle(0)
     right_motor.reset_angle(0)
     last_error = 0
@@ -22,25 +21,23 @@ def drive_general(distance, speed, set_point, absolute=True):
         hub.imu.reset_heading(0)
 
     distance_passed = calculate_distance()
-    
-    if speed <= 250:
-        final_speed = speed / 2
-    else:
-        final_speed = 100
-    
-    deceleration_starting_position = (distance / 3) * 2
+    final_speed = speed / 3
+
+    deceleration_starting_position = (distance / 3)*2
     deceleration_range = distance - deceleration_starting_position
     max_speed = speed
+
     while distance_passed < distance:
+        if distance > 150:
+            if distance_passed > deceleration_starting_position:
+                speed = (final_speed - max_speed) * ((distance_passed - deceleration_starting_position) / deceleration_range) + max_speed
+        else:
+            speed = max_speed
         distance_passed = calculate_distance()
         error = set_point - hub.imu.heading()
         p = error * kp
         d = (error - last_error) * kd
-        correction = p + d
-        if distance_passed < deceleration_starting_position:
-            speed = max_speed
-        else:
-            speed = (final_speed - max_speed) * ((distance_passed - deceleration_starting_position) / deceleration_range) + max_speed
+        correction = p + d            
         
         drive_base.drive(speed, correction)
     left_motor.brake()
