ec66ce2a6b6f1752008dd3314220738a14bc97ab
noaalfia <noaalfia2010@gmail.com>
2025-01-17 00:15:18 +0200

    Update run5.py

--- run5.py
+++ run5.py
@@ -1,21 +1,35 @@
-from parameters import left_arm_motor, right_arm_motor, hub, drive_base
+from parameters import left_arm_motor, right_arm_motor,hub,drive_base
 from pybricks.parameters import Stop
 from pybricks.tools import wait
-from drive_general import drive_general
 from turn_general import turn_general
+from drive_general import drive_general
 
-drive_general(880, 400 ,0, False)
-drive_general(50, 320 ,0, False)
-drive_general(200, -60 ,0, False)
-
-#do mission 8
-drive_general(100, -80 ,0, False)
-
-turn_general(-90)
-drive_general(100, -450, -90, True)
-turn_general(-65)
-drive_general(130, -300, 0, False)
-
-#put the ship in her place
-drive_general(235, -360, 0, False)
+#take shrimps and coral
 drive_general(100, 400, 0, False)
+turn_general(-36)
+drive_general(150, 400, -36, True)
+turn_general(37)
+left_arm_motor.run_angle(300, 400, Stop.BRAKE, False)
+drive_general(500, 600, 37, True)
+
+#mission 13
+drive_general(10, -200, 37, True)
+turn_general(25)
+drive_general(200, -400, 25, True)
+
+# take shrimp and Plankton Sample
+turn_general(37)
+left_arm_motor.run_angle(300, -500, Stop.BRAKE, True)
+drive_general(120, 400, 37, True)
+turn_general(25)
+drive_general(20, 250, 25, True)
+left_arm_motor.run_angle(300, 500, Stop.BRAKE, True)
+drive_general(20, -400, 25, True)
+
+#octupos
+turn_general(-70)
+drive_general(400, -500, -90, True)
+turn_general(-40)
+drive_general(200, 550, -40, True)
+drive_general(570, -600, 0, False)
+  
