45a5c703954696f40aac09da0a22bf433209a1b6
noaalfia <noaalfia2010@gmail.com>
2025-01-17 00:13:30 +0200

    Update run6.py

--- run6.py
+++ run6.py
@@ -1,35 +1,39 @@
-from parameters import left_arm_motor, right_arm_motor,hub,drive_base
+from parameters import left_arm_motor, right_arm_motor,hub,drive_base, right_motor, left_motor
 from pybricks.parameters import Stop
 from pybricks.tools import wait
 from turn_general import turn_general
 from drive_general import drive_general
 
-#take shrimps and coral
-drive_general(150, 500, 0, False)
-turn_general(-37)
-drive_general(150, 500, -37, True)
-turn_general(35)
-left_arm_motor.run_angle(300, 400, Stop.BRAKE, False)
-drive_general(500, 600, 35, True)
+#put the shark in place
+drive_general(400, 500, 0, False)
+turn_general(-12)
+drive_general(200, 400, -12, True)
+drive_general(140, 100, -12, True)
 
-#mission 13
-drive_general(10, -200, 35, True)
-turn_general(25)
-drive_general(200, -400, 25, True)
+drive_general(100, -400, -12, True)
+drive_general(100, -100, -12, True)
+turn_general(-110)
+drive_general(70, 150, -119, True)
+turn_general(10)
+drive_general(50, 600, 10, True)
+wait(500)
+drive_general(30, -300, 10, True)
+drive_general(60, 600, 10, True)
+wait(500)
+drive_general(40, -300, 10, True)
 
-# take shrimp and Plankton Sample
-turn_general(37)
-left_arm_motor.run_angle(300, -500, Stop.BRAKE, True)
-drive_general(120, 400, 37, True)
-turn_general(25)
-drive_general(20, 250, 25, True)
-left_arm_motor.run_angle(300, 500, Stop.BRAKE, True)
-drive_general(20, -400, 25, True)
 
-#octupos
-turn_general(-70)
-drive_general(400, -500, -90, True)
+#back to the wall
+turn_general(90)
+drive_general(250, -600, 90, True)
+turn_general(80)
+drive_general(200, -600, 0, False)
+drive_general(650, 700, 0, True)
+
+# do the ship
+right_arm_motor.run_angle(10000, 1300, Stop.BRAKE, True)
 turn_general(-40)
-drive_general(200, 550, -40, True)
-drive_general(600, -600, 0, False)
-  
+drive_general(90, -400, -50, True)
+right_arm_motor.run_angle(-10000, 1300, Stop.BRAKE, True)
+turn_general(35)
+drive_general(600, -1000, 35, True)
