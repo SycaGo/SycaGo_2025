1d0991ebc5e63d093af1a4799660a4baed4ae9cf
noaalfia <noaalfia2010@gmail.com>
2025-01-17 17:37:02 +0200

    Update run4.py

--- run4.py
+++ run4.py
@@ -3,7 +3,7 @@ from pybricks.parameters import Stop
 from pybricks.tools import wait
 from drive_general import drive_general
 from turn_general import turn_general
-drive_general(40, 200 ,0, False)
+drive_general(60, 200 ,0, False)
 turn_general(90)
 
 #do mission 8
@@ -11,12 +11,19 @@ drive_general(800, 400 ,90, True)
 drive_general(50, 320 ,90, True)
 drive_general(200, -60 ,90, True)
 drive_general(60, -200 ,90, True)
-turn_general(-60)
-drive_general(50, 200 ,-60, True)
-turn_general(-90)
-turn_general(20)
-drive_general(130, -300, 0, False)
+turn_general(-55)
+drive_general(80, 200 ,-55, True)
+
+#take the trident out
+turn_general(-70)
+
+#back to the wall
+drive_general(170, -450 ,-20, True)
+
+turn_general(-58)
+print(hub.imu.heading())
+drive_general(130, -200, 0, False)
 
 #put the ship in her place
-drive_general(300, -360, 0, False)
+drive_general(150, -300, 0, False)
 drive_general(100, 400, 0, False)
