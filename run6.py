f408f7fd5447776a3464f7ae64f56355417db31a
noaalfia <noaalfia2010@gmail.com>
2025-01-18 19:24:24 +0200

    Update run6.py

--- run6.py
+++ run6.py
@@ -4,36 +4,27 @@ from pybricks.tools import wait
 from turn_general import turn_general
 from drive_general import drive_general
 
-#put the shark in place
-drive_general(400, 500, 0, False)
-turn_general(-12)
-drive_general(200, 400, -12, True)
-drive_general(140, 100, -12, True)
+drive_general(350, 450, 0, False)
+turn_general(-15)
+drive_general(440, 400, -15, True)
 
-drive_general(100, -400, -12, True)
-drive_general(100, -100, -12, True)
-turn_general(-110)
-drive_general(70, 150, -119, True)
-turn_general(10)
-drive_general(50, 600, 10, True)
-wait(500)
-drive_general(30, -300, 10, True)
-drive_general(60, 600, 10, True)
-wait(500)
-drive_general(40, -300, 10, True)
-
+#put the shark in his place
+drive_general(300, -400, -15, True)
+turn_general(-100)
+drive_general(150, 400, -100, True)
+drive_general(40, -300, -100, True)
 
 #back to the wall
 turn_general(90)
-drive_general(250, -600, 90, True)
-turn_general(80)
-drive_general(200, -600, 0, False)
-drive_general(650, 700, 0, True)
+drive_general(200, -600, 90, True)
+turn_general(70)
+drive_general(200, -300, 0, False)
+drive_general(650, 700, 0, False)
 
 # do the ship
 right_arm_motor.run_angle(10000, 1300, Stop.BRAKE, True)
-turn_general(-40)
-drive_general(90, -400, -50, True)
+turn_general(-48)
+drive_general(90, -400, -48, True)
 right_arm_motor.run_angle(-10000, 1300, Stop.BRAKE, True)
 turn_general(35)
 drive_general(600, -1000, 35, True)
