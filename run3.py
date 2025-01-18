6dfd639c8b1cca685d6fd68fa6bd1ba46c731b95
noaalfia <noaalfia2010@gmail.com>
2025-01-18 14:14:58 +0200

    Update run3.py

--- run3.py
+++ run3.py
@@ -5,18 +5,29 @@ from drive_general import drive_general
 from turn_general import turn_general
 
 drive_general(110, 300, 0, False)
-turn_general(91)
-drive_general(700, -500, 91, True)
-turn_general(15)
-drive_general(110, -600, 15, True)
+turn_general(90)
+
+#go backward to mission 3
+drive_general(700, -350, 90, True)
+turn_general(17)
+drive_general(110, -600, 17, True)
+
+#put the scubadiver on the yellow line and click on the yellow
 left_arm_motor.run_angle(10000, 1000, Stop.BRAKE, True)
 left_arm_motor.run_angle(10000, -365, Stop.BRAKE, True)
 drive_general(300, 550, 0, False)
 turn_general(-90)
-drive_general(70, 150, -90, True)
+
+#drive straight to mission 6
+drive_general(80, 150, -90, True)
+
+#get up the toren
 drive_general(130, 70, -90, True)
-right_arm_motor.run_angle(100, 120, Stop.BRAKE, True)
-right_arm_motor.run_angle(500, 200, Stop.BRAKE, True)
+
+#take the srimp
+right_arm_motor.run_angle(1000, 220, Stop.BRAKE, True)
+
+#take the chest
 drive_general(100, -200, -90, True)
 turn_general(120)
 drive_general(440, 700, 120, True)
