1c87a1b6638da15200474e4239184dcf99d806cb
noaalfia <noaalfia2010@gmail.com>
2025-01-19 18:15:52 +0200

    Update run2.py

--- run2.py
+++ run2.py
@@ -4,37 +4,31 @@ from pybricks.tools import wait
 from drive_general import drive_general
 from turn_general import turn_general
 
-drive_general(100, 250, 0, False)
-turn_general(-25)
-drive_general(120, 300, -25, True)
-turn_general(-65)
-
-#backward to the wall
-drive_general(85, -200, 0, False)
+drive_general(250, 250, 0, False)
+turn_general(-90)
+drive_general(80, -250, 0, False)
 drive_general(600, 450, 0, False)
 turn_general(-90)
-left_arm_motor.run_angle(2500, -500, Stop.BRAKE,True)
+left_arm_motor.run_angle(2500, -700, Stop.BRAKE,True)
 
 #drive forward to mission 1 and take the scubadiver
 drive_general(140, 300, -90, True)
 
 left_arm_motor.run_angle(1800, 900, Stop.BRAKE, False)
-right_arm_motor.run_angle(3000, -2100, Stop.BRAKE,True)
+right_arm_motor.run_angle(6000, -2100, Stop.BRAKE,True)
 
 #return the arms
 right_arm_motor.run_angle(2000, 1100, Stop.BRAKE, True)
-left_arm_motor.run_angle(3000, -900, Stop.BRAKE, True)
-drive_general(120, -350, -90, True)
-left_arm_motor.run_angle(3000, 500, Stop.BRAKE, True)
-turn_general(70)
-drive_general(70, 300, 70, True)
+left_arm_motor.run_angle(3000, -1000, Stop.BRAKE, True)
+drive_general(100, -350, -90, True)
+left_arm_motor.run_angle(3000, 600, Stop.BRAKE, True)
+turn_general(80)
+drive_general(70, 300, 80, True)
+
 #drop the shark
 left_arm_motor.run_angle(10000, -800, Stop.BRAKE, True)
-drive_general(20, -350, 70, True)
-left_arm_motor.run_angle(10000, -100, Stop.BRAKE, True)
-wait(300)
-drive_general(40, -350, 70, True)
-left_arm_motor.run_angle(10000, 800, Stop.BRAKE, True)
+drive_general(35, -350, 80, True)
+left_arm_motor.run_angle(10000, 900, Stop.BRAKE, True)
 
 #backward to the wall
 turn_general(40)
