e135f3a09602b253e979f3d28ccdd66c1569536d
noaalfia <noaalfia2010@gmail.com>
2025-01-19 15:55:13 +0200

    Update run5.py

--- run5.py
+++ run5.py
@@ -5,22 +5,23 @@ from turn_general import turn_general
 from drive_general import drive_general
 
 #take shrimps and coral
-drive_general(100, 400, 0, False)
+drive_general(100, 250, 0, False)
 turn_general(-36)
-drive_general(150, 400, -36, True)
-turn_general(37)
+drive_general(150, 250, -36, True)
+turn_general(35)
 left_arm_motor.run_angle(300, 400, Stop.BRAKE, False)
-drive_general(500, 600, 37, True)
+drive_general(500, 500, 35, True)
+print(hub.imu.heading())
 
 #mission 13
-drive_general(10, -200, 37, True)
+drive_general(10, -200, 35, True)
 turn_general(25)
 drive_general(200, -400, 25, True)
 
 # take shrimp and Plankton Sample
-turn_general(37)
+turn_general(35)
 left_arm_motor.run_angle(300, -500, Stop.BRAKE, True)
-drive_general(120, 400, 37, True)
+drive_general(120, 400, 35, True)
 turn_general(25)
 drive_general(20, 250, 25, True)
 left_arm_motor.run_angle(300, 500, Stop.BRAKE, True)
@@ -30,6 +31,6 @@ drive_general(20, -400, 25, True)
 turn_general(-70)
 drive_general(400, -500, -90, True)
 turn_general(-40)
-drive_general(200, 550, -40, True)
+drive_general(200, 500, -40, True)
 drive_general(570, -600, 0, False)
   
