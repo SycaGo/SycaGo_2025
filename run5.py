c553674c57e975dd5e3e7bcc492f6a1bbec32552
noaalfia <noaalfia2010@gmail.com>
2025-01-22 23:48:25 +0200

    Update run5.py

--- run5.py
+++ run5.py
@@ -5,31 +5,29 @@ from turn_general import turn_general
 from drive_general import drive_general
 
 #take shrimps and coral
-drive_general(150, 250, 0, False)
-turn_general(-37)
-drive_general(150, 250, -37, True)
-turn_general(35)
-left_arm_motor.run_angle(300, 400, Stop.BRAKE, False)
-drive_general(520, 500, 35, True)
+drive_general(150, 200, 0, False)
+turn_general(-45)
+drive_general(200, 200, -45, True)
+turn_general(40)
+right_arm_motor.run_angle(300, -400, Stop.BRAKE, False)
+drive_general(520, 500, 40, True)
 
-#mission 13
-drive_general(10, -200, 35, True)
-turn_general(25)
-drive_general(200, -400, 25, True)
+#mission 11
+left_arm_motor.run_angle(600, -700, Stop.BRAKE, True)
 
 # take shrimp and Plankton Sample
-turn_general(35)
-left_arm_motor.run_angle(300, -500, Stop.BRAKE, True)
-drive_general(120, 400, 35, True)
-turn_general(25)
-drive_general(20, 250, 25, True)
-left_arm_motor.run_angle(300, 500, Stop.BRAKE, True)
-drive_general(20, -400, 25, True)
+drive_general(300, -300, 0, False)
+right_arm_motor.run_angle(1000, 400, Stop.BRAKE, True)
+turn_general(30)
+drive_general(80, 400, 30, True)
+turn_general(30)
+drive_general(10, 300, 30, True)
+right_arm_motor.run_angle(600, -400, Stop.BRAKE, True)
+drive_general(30, -400, 30, True)
+turn_general(-80)
+drive_general(300, -400, -80, True)
+turn_general(-50)
 
 #octupos
-turn_general(-70)
-drive_general(400, -500, -90, True)
-turn_general(-40)
-drive_general(200, 500, -40, True)
-drive_general(570, -600, 0, False)
-  
+drive_general(200, 300, -50, True)
+drive_general(500, -2000, -50, True)
