c2ba750c7c331f4dccfe9aca990173f01b96f321
noaalfia <noaalfia2010@gmail.com>
2025-01-23 20:07:02 +0200

    change the numbers

--- run3.py
+++ run3.py
@@ -8,25 +8,25 @@ drive_general(120, 300, 0, False)
 turn_general(93)
 
 #go backward to mission 3
-drive_general(800, -350, 93, True)
+drive_general(780, -350, 93, True)
 
 #put the scubadiver on the yellow line and click on the yellow
 left_arm_motor.run_angle(10000, 1000, Stop.BRAKE, True)
 left_arm_motor.run_angle(10000, -365, Stop.BRAKE, True)
-drive_general(300, 550, 93, True)
+drive_general(350, 400, 0, False)
 turn_general(-90)
 
 #drive straight to mission 6
-drive_general(80, 150, -90, True)
+drive_general(85, 150, 0, False)
 
 #get up the toren
-drive_general(130, 70, -90, True)
+drive_general(130, 70, 0, False)
 
 #take the srimp
 right_arm_motor.run_angle(350, 450, Stop.BRAKE, True)
 
 #take the chest
-drive_general(100, -200, -90, True)
+drive_general(100, -200, 0, False)
 turn_general(120)
 drive_general(440, 1000, 120, True)
 
