1c2d0810f4a66f7da27a9fc5b4ab52ed1f83d789
noaalfia <noaalfia2010@gmail.com>
2025-01-25 19:58:36 +0200

    Update PID_drive.py

--- PID_drive.py
+++ PID_drive.py
@@ -9,30 +9,34 @@ def calculate_distance():
     return distance_driven
 
 def drive(distance, speed, set_point, absolute=True):
-    set_point = 0
-    kp = 1
+    kp = 0.7
+    kd = 2
     left_motor.reset_angle(0)
     right_motor.reset_angle(0)
-    hub.imu.reset_heading(0)
-    scale = abs(set_point / 10)
+    last_error = 0
+    if not(absolute):
+        hub.imu.reset_heading(0)
+
     distance_passed = calculate_distance()
-    
-    final_speed = speed / 2
-    deceleration_starting_position = (distance / 3) * 2
+    final_speed = speed / 3
+
+    deceleration_starting_position = (distance / 3)*2
     deceleration_range = distance - deceleration_starting_position
     max_speed = speed
+
     while distance_passed < distance:
+        if distance > 150:
+            if distance_passed > deceleration_starting_position:
+                speed = (final_speed - max_speed) * ((distance_passed - deceleration_starting_position) / deceleration_range) + max_speed
+        else:
+            speed = max_speed
         distance_passed = calculate_distance()
         error = set_point - hub.imu.heading()
         p = error * kp
-        correction = p
-        if distance_passed < deceleration_starting_position:
-            speed = max_speed
-        else:
-            speed = (final_speed - max_speed) * ((distance_passed - deceleration_starting_position) / deceleration_range) + max_speed
+        d = (error - last_error) * kd
+        correction = p + d            
         
         drive_base.drive(speed, correction)
     left_motor.brake()
     right_motor.brake()
     wait(100)
-
