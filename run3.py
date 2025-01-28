f8dc01bbb087af5c7104465d2e19c2c06ef3035c
noaalfia <noaalfia2010@gmail.com>
2025-01-28 16:09:05 +0200

    Update run3.py

--- run3.py
+++ run3.py
@@ -4,7 +4,7 @@ from pybricks.tools import wait
 from drive_general import drive_general
 from turn_general import turn_general
 
-drive_general(120, 300, 0, False)
+drive_general(125, 300, 0, False)
 turn_general(93)
 
 #go backward to mission 3
@@ -15,12 +15,11 @@ left_arm_motor.run_angle(10000, 1000, Stop.BRAKE, True)
 left_arm_motor.run_angle(10000, -365, Stop.BRAKE, True)
 drive_general(350, 400, 0, False)
 turn_general(-90)
-
 #drive straight to mission 6
 drive_general(85, 150, 0, False)
 
 #get up the toren
-drive_general(130, 70, 0, False)
+drive_general(160, 70, 0, False)
 
 #take the srimp
 right_arm_motor.run_angle(350, 450, Stop.BRAKE, True)
@@ -28,5 +27,4 @@ right_arm_motor.run_angle(350, 450, Stop.BRAKE, True)
 #take the chest
 drive_general(100, -200, 0, False)
 turn_general(120)
-drive_general(440, 1000, 120, True)
-
+drive_general(440, 2000, 120, True)
