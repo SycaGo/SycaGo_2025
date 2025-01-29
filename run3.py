c71121c22a3e64034e7c825ed76732638909900d
noaalfia <noaalfia2010@gmail.com>
2025-01-29 23:03:31 +0200

    Update run3.py

--- run3.py
+++ run3.py
@@ -4,27 +4,29 @@ from pybricks.tools import wait
 from drive_general import drive_general
 from turn_general import turn_general
 
-drive_general(125, 300, 0, False)
-turn_general(93)
-
-#go backward to mission 3
-drive_general(780, -350, 93, True)
-
-#put the scubadiver on the yellow line and click on the yellow
-left_arm_motor.run_angle(10000, 1000, Stop.BRAKE, True)
-left_arm_motor.run_angle(10000, -365, Stop.BRAKE, True)
-drive_general(350, 400, 0, False)
-turn_general(-90)
 #drive straight to mission 6
-drive_general(85, 150, 0, False)
+drive_general(120, 350, 0, False)
+turn_general(-90)
+wait(100)
+drive_general(370, 350, -90, True)
+turn_general(90)
+wait(100)
 
 #get up the toren
-drive_general(160, 70, 0, False)
+drive_general(150, 110, 90, True)
+drive_general(80, 50, 90, True)
 
 #take the srimp
-right_arm_motor.run_angle(350, 450, Stop.BRAKE, True)
+right_arm_motor.run_angle(350, 400, Stop.BRAKE, True)
 
 #take the chest
-drive_general(100, -200, 0, False)
-turn_general(120)
-drive_general(440, 2000, 120, True)
+drive_general(180, -350, 90, True)
+
+#go backward to mission 3
+turn_general(93)
+drive_general(400, -350, 93, True)
+left_arm_motor.run_angle(10000, 1000, Stop.BRAKE, True)
+left_arm_motor.run_angle(10000, -365, Stop.BRAKE, True)
+drive_general(500, 1000, 93, True)
+turn_general(50)
+drive_general(300, 1000, 50 , True)
