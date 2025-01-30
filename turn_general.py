eb518881f39a3fe15a4b33fc3d42bbf9dda713a5
noaalfia <noaalfia2010@gmail.com>
2025-01-31 00:13:43 +0200

    added the option to do absolute turn

--- turn_general.py
+++ turn_general.py
@@ -2,19 +2,22 @@ from parameters import left_motor, right_motor, hub, drive_base
 from pybricks.parameters import Stop
 from pybricks.tools import wait
 
-def turn_general(turn_degrees, Kp = 8, Ki = 0.009, Kd = 2):
-
-    hub.imu.reset_heading(0)
+def turn_general(turn_degrees, absolute=True, Kp = 8, Ki = 0.009, Kd = 2):
     error = turn_degrees - hub.imu.heading()
     integral = 0
     last_error = 0
     scale = abs(turn_degrees / 10)
+
+    if not absolute:
+        hub.imu.reset_heading(0)    
+
     while abs(error) > 0.5:
         gyro = hub.imu.heading()
         error = turn_degrees - gyro
         p = error * Kp
         d = (error - last_error) * Kd
         correction = p + d
+
         if gyro >= abs(turn_degrees) - scale:
             integral += error
             i = integral * Ki
