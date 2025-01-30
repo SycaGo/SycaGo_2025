5aa02ef6f20881a2c39de6d44a2fdea96f89da7a
noaalfia <noaalfia2010@gmail.com>
2025-01-31 00:11:21 +0200

    added absolute turns

--- run5.py
+++ run5.py
@@ -1,23 +1,37 @@
-from parameters import left_arm_motor, right_arm_motor, hub, drive_base
+from parameters import left_arm_motor, right_arm_motor,hub,drive_base
 from pybricks.parameters import Stop
 from pybricks.tools import wait
-from drive_general import drive_general
 from turn_general import turn_general
+from drive_general import drive_general
 
-drive_general(60, 200 ,0, False)
-turn_general(90)
+#take shrimps and coral
+drive_general(150, 350, 0, False)
+turn_general(-45)
+drive_general(200, 350, -45, True)
+turn_general(-5)
+right_arm_motor.run_angle(300, -400, Stop.BRAKE, False)
+drive_general(300, 400, -5, True)
+wait(200)
+drive_general(180, 300, -5, True)
 
-#do mission 8
-drive_general(780, 500 ,90, True)
-drive_general(90, 400 ,90, True)
-wait(400)
-drive_general(100, -200 ,90, True)
-drive_general(100, -30 ,90, True)
-drive_general(20, -300 ,90, True)
-turn_general(-60, False)
-drive_general(200, 350 ,-60, True)
-turn_general(40, False)
-left_arm_motor.run_angle(1000, 300, Stop.BRAKE, True)
+#mission 11
+left_arm_motor.run_angle(600, -350, Stop.BRAKE, True)
+drive_general(40, 300, -5, True)
+left_arm_motor.run_angle(600, -350, Stop.BRAKE, True)
 
-#get out the trident
-drive_general(150, -1000 ,40, True)
+# take shrimp and Plankton Sample
+drive_general(280, -300, -5, True)
+right_arm_motor.run_angle(1000, 400, Stop.BRAKE, True)
+turn_general(33, False)
+drive_general(60, 400, 33, True)
+turn_general(63)
+drive_general(10, 300, 63, True)
+right_arm_motor.run_angle(600, -400, Stop.BRAKE, True)
+drive_general(30, -450, 63, True)
+turn_general(-80, False)
+drive_general(330, -500, -80, True)
+turn_general(-120)
+
+#octupos
+drive_general(250, 500, -120, True)
+drive_general(500, -1000, -120, True)
