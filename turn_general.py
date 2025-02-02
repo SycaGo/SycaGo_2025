a00495aa4d331490fbfc4b844b077f0ceed6bcb6
noaalfia <noaalfia2010@gmail.com>
2025-02-02 16:03:54 +0200

    Update turn_general.py

--- turn_general.py
+++ turn_general.py
@@ -2,7 +2,7 @@ from parameters import left_motor, right_motor, hub, drive_base
 from pybricks.parameters import Stop
 from pybricks.tools import wait
 
-def turn_general(turn_degrees, Kp = 8, Ki = 0.009, Kd = 2):
+def turn_general(turn_degrees, Kp = 0, Ki = 0.009, Kd = 0):
     error = turn_degrees - hub.imu.heading()
     integral = 0
     last_error = 0
@@ -11,14 +11,15 @@ def turn_general(turn_degrees, Kp = 8, Ki = 0.009, Kd = 2):
         error = turn_degrees - gyro
         p = error * Kp
         d = (error - last_error) * Kd
-        integral += error
-        correction = p + d
 
         if abs(error) > 5:
             integral = 0
-        elif abs(error) <= 5:
-            i = integral * Ki
-            correction = p + i + d
+        else:
+            integral += error
+
+        i = integral * Ki
+        correction = p + i + d
+
         last_error = error
         left_motor.run(correction)
         right_motor.run(-correction)
