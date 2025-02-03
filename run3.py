e2e9f41c7f575339120fe2a482cccf7960d52d4d
noaalfia <noaalfia2010@gmail.com>
2025-02-03 12:48:37 +0200

    Update run3.py

--- run3.py
+++ run3.py
@@ -8,7 +8,7 @@ from turn_general import turn_general
 drive_general(120, 350, 0, False)
 turn_general(-90)
 wait(100)
-drive_general(370, 350, -90, True)
+drive_general(390, 350, -90, True)
 turn_general(90, False, Kp= 6)
 wait(100)
 
@@ -17,7 +17,7 @@ drive_general(180, 110, 90, True)
 drive_general(80, 50, 90, True)
 
 #take the srimp
-right_arm_motor.run_angle(350, 400, Stop.BRAKE, True)
+right_arm_motor.run_angle(350, 430, Stop.BRAKE, True)
 
 #take the chest
 drive_general(180, -350, 90, True)
@@ -29,7 +29,7 @@ right_arm_motor.run_angle(350, -400, Stop.BRAKE, True)
 #go backward to mission 3
 drive_general(380, -350, 183, True)
 left_arm_motor.run_angle(10000, 1000, Stop.BRAKE, True)
-left_arm_motor.run_angle(10000, -120, Stop.BRAKE, True)
+left_arm_motor.run_angle(10000, -1, Stop.BRAKE, True)
 drive_general(500, 1000, 183, True)
 turn_general(50, False)
 drive_general(300, 1000, 50 , True)
