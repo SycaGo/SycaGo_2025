dd73ef44f81609c8ed56272cb45e1c599415e5ab
noaalfia <noaalfia2010@gmail.com>
2025-02-07 09:53:22 +0200

    Update run5.py

--- run5.py
+++ run5.py
@@ -20,16 +20,16 @@ drive_general(40, 300, -5, True)
 left_arm_motor.run_angle(600, 350, Stop.BRAKE, True)
 
 # take shrimp and Plankton Sample
-drive_general(280, -300, -5, True)
+drive_general(220, -300, -5, True)
 right_arm_motor.run_angle(1000, 400, Stop.BRAKE, True)
 turn_general(33, False)
-drive_general(60, 400, 33, True)
-turn_general(63)
+drive_general(40, 400, 33, True)
+turn_general(73)
 drive_general(10, 300, 63, True)
 right_arm_motor.run_angle(600, -400, Stop.BRAKE, True)
-drive_general(30, -450, 63, True)
+drive_general(60, -450, 63, True)
 turn_general(-80, False)
-drive_general(330, -500, -80, True)
+drive_general(370, -500, -80, True)
 turn_general(-120)
 
 #octupos
