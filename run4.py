f51cfd1499b9725b06e979afcd46570a642b00f0
noaalfia <noaalfia2010@gmail.com>
2025-02-10 19:33:57 +0200

    add more angle before mission 14D

--- run4.py
+++ run4.py
@@ -4,7 +4,7 @@ from pybricks.tools import wait
 from drive_general import drive_general
 from turn_general import turn_general
 
-drive_general(60, 200 ,0, False)
+drive_general(100, 200 ,0, False)
 turn_general(90)
 
 #do mission 8
@@ -14,10 +14,10 @@ wait(400)
 drive_general(100, -200 ,90, True)
 drive_general(100, -30 ,90, True)
 drive_general(20, -300 ,90, True)
-turn_general(-60, False)
-drive_general(200, 350 ,-60, True)
+turn_general(-52, False)
+drive_general(220, 350 ,-52, True)
 turn_general(40, False)
 left_arm_motor.run_angle(1000, 300, Stop.BRAKE, True)
 
 #get out the trident
-drive_general(150, -1000 ,40, True)
+drive_general(250, -1000 ,40, True)
