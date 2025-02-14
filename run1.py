fd96d3beedecfe7ee688773e13c497cf6c43ced4
noaalfia <noaalfia2010@gmail.com>
2025-02-14 14:51:47 +0200

    Update run1.py
    We changed the angle ruler

--- run1.py
+++ run1.py
@@ -4,13 +4,13 @@ from pybricks.tools import wait
 from turn_general import turn_general
 from drive_general import drive_general
 
-drive_general(640, 500, 0, False)
+drive_general(680, 500, 0, False)
 turn_general(24)
 right_arm_motor.run_angle(300, 330, Stop.BRAKE, False)
-drive_general(280, 450, 24, True)
+drive_general(310, 450,33, True)
 turn_general(144)
-drive_general(40, 200, 144, True)
-left_arm_motor.run_angle(600, 280, Stop.BRAKE, True)
+drive_general(20, 200, 144, True)
+left_arm_motor.run_angle(650, 280, Stop.BRAKE, True)
 drive_general(100, -400, 144, True)
 left_arm_motor.run_angle(300, -250, Stop.BRAKE, True)
 drive_general(50, 200, 0, False)
