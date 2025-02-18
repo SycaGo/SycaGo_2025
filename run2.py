9f876c8195c1c678e0fc63a62138a98e763dc46e
noaalfia <noaalfia2010@gmail.com>
2025-02-18 22:17:47 +0200

    Update run2.py

--- run2.py
+++ run2.py
@@ -4,34 +4,29 @@ from pybricks.tools import wait
 from drive_general import drive_general
 from turn_general import turn_general
 
-drive_general(250, 250, 0, False)
-turn_general(-90)
-drive_general(100, -250, -90, True)
-drive_general(600, 450, -90, True)
-turn_general(-180)
-left_arm_motor.run_angle(2500, -710, Stop.BRAKE,True)
+#put the coral on the yellow
+drive_general(280, 350, 0, False)
+left_arm_motor.run_angle(300, -120, Stop.BRAKE,True)
+drive_general(10, -165, 0, False)
+wait(50)
+left_arm_motor.run_angle(6000, -345, Stop.BRAKE,True)
+drive_general(180, 350, 0, False)
+turn_general(-10)
+left_arm_motor.run_angle(50, 80, Stop.BRAKE,True)
+left_arm_motor.run_angle(20, -80, Stop.BRAKE,True)
+drive_general(100, -200, -10, True)
 
-#drive forward to mission 1 and take the scubadiver
-drive_general(140, 300, -180, True)
-
-left_arm_motor.run_angle(1800, 880, Stop.BRAKE, False)
-right_arm_motor.run_angle(400, -400, Stop.BRAKE,True)
-right_arm_motor.run_angle(4000, -1600, Stop.BRAKE,True)
-
-
-#return the arms
-right_arm_motor.run_angle(2000, 1500, Stop.BRAKE, True)
-left_arm_motor.run_angle(3000, -1000, Stop.BRAKE, True)
-drive_general(120, -350, -180, True)
-left_arm_motor.run_angle(3000, 600, Stop.BRAKE, True)
-turn_general(80, False)
-drive_general(50, 350, 80, True)
-
-#drop the shark
-left_arm_motor.run_angle(10000, -800, Stop.BRAKE, True)
-drive_general(35, -350, 80, True)
-left_arm_motor.run_angle(10000, 900, Stop.BRAKE, True)
-
-#backward to the wall
-turn_general(40, False)
-drive_general(730, -1000, 40, True)
+#free shark
+turn_general(60)
+drive_general(300, 350, 60, True)
+turn_general(-10)
+drive_general(200, 350, -10, True)
+turn_general(-47)
+drive_general(16, 500, -47, True)
+left_arm_motor.run_angle(10000,700, Stop.BRAKE,True)
+#corals and diver
+drive_general(150, -350, -47, True)
+left_arm_motor.run_angle(250, -600, Stop.BRAKE,False)
+turn_general(-35, False)
+drive_general(150, 350, -35, True)
+right_arm_motor.run_angle(100, -200, Stop.BRAKE,True)
