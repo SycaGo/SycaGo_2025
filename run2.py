843ee495d461bdfb92cff6d07213b96ad58a74bf
noaalfia <noaalfia2010@gmail.com>
2025-02-19 23:12:27 +0200

    Update run2.py

--- run2.py
+++ run2.py
@@ -4,29 +4,40 @@ from pybricks.tools import wait
 from drive_general import drive_general
 from turn_general import turn_general
 
-#put the coral on the yellow
-drive_general(280, 350, 0, False)
-left_arm_motor.run_angle(320, -120, Stop.BRAKE,True)
-drive_general(10, -165, 0, False)
-wait(50)
-left_arm_motor.run_angle(6000, -340, Stop.BRAKE,True)
-drive_general(180, 350, 0, False)
-turn_general(-10)
-left_arm_motor.run_angle(50, 80, Stop.BRAKE,True)
-left_arm_motor.run_angle(20, -80, Stop.BRAKE,True)
-drive_general(100, -200, -10, True)
+#run from the base to the coral
+drive_general(280, 340, 0, False)
 
-#free shark
-turn_general(60)
-drive_general(300, 350, 60, True)
-turn_general(-10)
-drive_general(200, 350, -10, True)
-turn_general(-47)
-drive_general(16, 500, -47, True)
-left_arm_motor.run_angle(10000,700, Stop.BRAKE,True)
-#corals and diver
-drive_general(150, -350, -47, True)
-left_arm_motor.run_angle(250, -600, Stop.BRAKE,False)
-turn_general(-35, False)
-drive_general(150, 350, -35, True)
-right_arm_motor.run_angle(100, -200, Stop.BRAKE,True)
+#catch the ring
+left_arm_motor.run_angle(320, -350, Stop.BRAKE,True)
+
+#take the coral
+left_arm_motor.run_angle(6000, -380, Stop.BRAKE,False)
+drive_general(30, -165, 0, True)
+
+wait(50)
+drive_general(165, 300, 0, True)
+turn_general(20)
+
+#put the coral on the yellow
+left_arm_motor.run_angle(6000, 320, Stop.BRAKE,True)
+drive_general(40, -250, 20, True)
+
+#drive to the next mission
+turn_general(70)
+drive_general(280, 350, 70, True)
+turn_general(25)
+drive_general(180, 350, 25, True)
+turn_general(-48)
+drive_general(140, 350, -48, True)
+
+#free the shark
+left_arm_motor.run_angle(10000, 635, Stop.BRAKE,True)
+
+#go to mission 1
+drive_general(150, -350, -48, True)
+left_arm_motor.run_angle(6000, -820, Stop.BRAKE,True)
+turn_general(-90)
+
+
+drive_general(130, 350, -90, True)
+right_arm_motor.run_angle(3000, -250, Stop.BRAKE,True)
