a947210b87f014f5ba1c96648e39331d5d187902
noaalfia <noaalfia2010@gmail.com>
2025-02-21 14:43:39 +0200

    only add comments to run 1

--- run1.py
+++ run1.py
@@ -4,13 +4,22 @@ from pybricks.tools import wait
 from turn_general import turn_general
 from drive_general import drive_general
 
+# taking 2 corals shrimp and one sample
 drive_general(640, 500, 0, False)
 turn_general(24)
+
+# closing the cage and driving to the next sample
 right_arm_motor.run_angle(300, 330, Stop.BRAKE, False)
 drive_general(280, 450, 24, True)
+
+# turning to take the sample
 turn_general(144)
-drive_general(40, 200, 144, True)
+drive_general(40, 200, 144, True) 
+
+# take the sample
 left_arm_motor.run_angle(600, 280, Stop.BRAKE, True)
+
+# driving back to launch area
 drive_general(100, -400, 144, True)
 left_arm_motor.run_angle(300, -250, Stop.BRAKE, True)
 drive_general(50, 200, 0, False)
