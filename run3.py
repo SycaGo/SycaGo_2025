06e26567783533d0ab8c46d4ff26009cb6976772
noaalfia <noaalfia2010@gmail.com>
2025-02-27 19:22:51 +0200

    added

--- run3.py
+++ run3.py
@@ -10,35 +10,35 @@ turn_general(-90)
 wait(100)
 
 #drive straight to the Toren
-drive_general(420, 350, -90, True)
+drive_general(440, 350, -90, True)
 
 #turn to alllign with the Toren
-turn_general(90, False, Kp= 6)
+turn_general(0, Kp= 6)
 wait(100)
 
 #get up the toren
-drive_general(180, 110, 90, True)
-drive_general(80, 50, 90, True)
+drive_general(180, 110, 0, True)
+drive_general(80, 50, 0, True)
 
 #take the srimp/ krill
 right_arm_motor.run_angle(350, 430, Stop.BRAKE, True)
 
 #take the chest
-drive_general(180, -350, 90, True)
+drive_general(160, -350, 0, True)
 
 # release the shrimp in launch area
-turn_general(185)
+turn_general(93)
 wait(500)
 right_arm_motor.run_angle(350, -400, Stop.BRAKE, True)
 
 #go backwards to the coral reef and the scuba diver hanger
-drive_general(380, -350, 185, True)
+drive_general(380, -350, 93, True)
 
 # put the Scuba diver in the hanger and put down the coral reef
 left_arm_motor.run_angle(10000, 1000, Stop.BRAKE, True)
 left_arm_motor.run_angle(10000, -200, Stop.BRAKE, True)
 
 #get back to launch area
-drive_general(500, 1000, 185, True)
-turn_general(50, False)
-drive_general(300, 1000, 50, True)
+drive_general(500, 1000, 93, True)
+turn_general(143)
+drive_general(300, 1000, 143, True)
