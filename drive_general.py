683038bfe8b54d5f4b27244f73d2d836ecaf06ab
noaalfia <noaalfia2010@gmail.com>
2025-03-03 15:58:25 +0200

    added Units of measure to speed and distance

--- drive_general.py
+++ drive_general.py
@@ -12,9 +12,7 @@ def calculate_distance():
     distance_driven = ((average_angle / 360) * P)
     return distance_driven
 
-def drive_general(distance, speed, direction_angle, absolute=True, kp = 0.7, kd = 2):
-    # distance is in millimeters
-    # speed is in degrees (rotations) per second
+def drive_general(distance_mm, speed_deg_s, direction_angle, absolute=True, kp = 0.7, kd = 2):
     left_motor.reset_angle(0)
     right_motor.reset_angle(0)
     last_error = 0
@@ -22,25 +20,25 @@ def drive_general(distance, speed, direction_angle, absolute=True, kp = 0.7, kd 
         hub.imu.reset_heading(0)
 
     distance_passed = calculate_distance()
-    final_speed = speed / 3
+    final_speed = speed_deg_s / 3
 
-    deceleration_starting_position = (distance / 3)*2
-    deceleration_range = distance - deceleration_starting_position
-    max_speed = speed
+    deceleration_starting_position = (distance_mm / 3)*2
+    deceleration_range = distance_mm - deceleration_starting_position
+    max_speed = speed_deg_s
 
-    while distance_passed < distance:
-        if distance > 150 or speed != 1000 or speed != -1000:
+    while distance_passed < distance_mm:
+        if distance_mm > 150 or speed_deg_s != 1000 or speed_deg_s != -1000:
             if distance_passed > deceleration_starting_position:
-                speed = (final_speed - max_speed) * ((distance_passed - deceleration_starting_position) / deceleration_range) + max_speed
+                speed_deg_s = (final_speed - max_speed) * ((distance_passed - deceleration_starting_position) / deceleration_range) + max_speed
         else:
-            speed = max_speed
+            speed_deg_s = max_speed
         distance_passed = calculate_distance()
         error = direction_angle - hub.imu.heading()
         p = error * kp
         d = (error - last_error) * kd
         correction = p + d            
         
-        drive_base.drive(speed, correction)
+        drive_base.drive(speed_deg_s, correction)
     left_motor.brake()
     right_motor.brake()
     wait(100)
