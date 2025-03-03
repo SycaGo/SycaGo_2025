30b7595299b48cae703fcfbcad03bcc73497e2af
noaalfia <noaalfia2010@gmail.com>
2025-03-03 17:05:22 +0200

    add comments

--- drive_general.py
+++ drive_general.py
@@ -2,7 +2,7 @@ from parameters import left_motor, right_motor, hub, drive_base, WHEEL_DIAMETER
 from pybricks.parameters import Stop
 from pybricks.tools import wait
 
-
+# Distance calculation function
 def calculate_distance():
     PI = 3.141592
     right_motor_angle = right_motor.angle()
@@ -12,26 +12,33 @@ def calculate_distance():
     distance_driven = ((average_angle / 360) * P)
     return distance_driven
 
+# drive function
 def drive_general(distance_mm, speed_deg_s, direction_angle, absolute=True, kp = 0.7, kd = 2):
     left_motor.reset_angle(0)
     right_motor.reset_angle(0)
     last_error = 0
+
+    # Absolute navigation
     if not(absolute):
         hub.imu.reset_heading(0)
 
     distance_passed = calculate_distance()
-    final_speed = speed_deg_s / 3
 
+    # Local variables for the deceleration part
+    final_speed = speed_deg_s / 3
     deceleration_starting_position = (distance_mm / 3)*2
     deceleration_range = distance_mm - deceleration_starting_position
     max_speed = speed_deg_s
-
+    
+    # deceleration
     while distance_passed < distance_mm:
         if distance_mm > 150 or speed_deg_s != 1000 or speed_deg_s != -1000:
             if distance_passed > deceleration_starting_position:
                 speed_deg_s = (final_speed - max_speed) * ((distance_passed - deceleration_starting_position) / deceleration_range) + max_speed
-        else:
+        else:     
             speed_deg_s = max_speed
+
+        # PD part
         distance_passed = calculate_distance()
         error = direction_angle - hub.imu.heading()
         p = error * kp
