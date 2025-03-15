d97751c08e0aa92d78e3b4759b5b5f5b2cff65d5
noaalfia <noaalfia2010@gmail.com>
2025-03-15 10:39:54 +0200

    fix the comments

--- run2.py
+++ run2.py
@@ -4,25 +4,24 @@ from pybricks.tools import wait
 from drive_general import drive_general
 from turn_general import turn_general
 
-#run from the base to the coral
+# run from the base to the coral
 drive_general(280, 340, 0, False)
 
-#catch the ring
+# catch the ring
 left_arm_motor.run_angle(320, -350, Stop.BRAKE,True)
 
-#take the coral
+# take the coral
 left_arm_motor.run_angle(6000, -380, Stop.BRAKE,False)
 drive_general(30, -165, 0, True)
-
 wait(50)
 drive_general(165, 300, 0, True)
 turn_general(20)
 
-#put the coral on the yellow
+# put the coral on the yellow
 left_arm_motor.run_angle(6000, 320, Stop.BRAKE,True)
 drive_general(40, -250, 20, True)
 
-#drive to the next mission
+# drive to the shark
 turn_general(70)
 drive_general(280, 350, 70, True)
 turn_general(25)
@@ -30,17 +29,19 @@ drive_general(180, 350, 25, True)
 turn_general(-48)
 drive_general(150, 350, -48, True)
 
-#free the shark
+# free the shark
 left_arm_motor.run_angle(10000, 750, Stop.BRAKE,True)
 
-#go to mission 1
+# go to mission coral
 drive_general(140, -300, -48, True)
 left_arm_motor.run_angle(3000, -820, Stop.BRAKE,True)
 turn_general(-90)
 
-
+# free the diver
 drive_general(130, 350, -90, True)
 right_arm_motor.run_angle(4500, -230, Stop.BRAKE,True)
 drive_general(110, -350, -90, True)
+
+# back to launch area
 turn_general(-148)
 drive_general(600, 650, -148, True)
