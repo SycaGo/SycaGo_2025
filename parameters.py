5e34f188a9c129a3172934aafc812a82a57afdae
noaalfia <noaalfia2010@gmail.com>
2025-03-15 10:34:08 +0200

    added comment to the fanction run_angle

--- parameters.py
+++ parameters.py
@@ -13,6 +13,7 @@ AXLE_TRACK = 110
 drive_base = DriveBase(left_motor, right_motor, WHEEL_DIAMETER, AXLE_TRACK)
 drive_base.use_gyro(True)
 
+# left/right_arm_motor.run_angle(speed_deg/s, rotation_angle_deg, Stop.BRAKE, wait(bool))
 left_arm_motor = Motor(Port.D, Direction.COUNTERCLOCKWISE)
 right_arm_motor = Motor(Port.C)
 
