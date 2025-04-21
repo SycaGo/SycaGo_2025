99518093d083a30e9d9748ac9b9801047c5394f6
noaalfia <noaalfia2010@gmail.com>
2025-04-21 08:22:00 +0300

    Update run6.py

--- run6.py
+++ run6.py
@@ -18,20 +18,27 @@ drive_general(20, -200, 44, True)
 drive_general(50, 400, 44, True)
 wait(500)
 
+#leave the reef segments behind
+left_arm_motor.run_angle(3000, 1000, Stop.BRAKE,True)
+
+
 # pushing the fish
 drive_general(270, -300, 44, True)
 turn_general(-51, Kp=7)
-drive_general(150, 500, -51, True)
+drive_general(170, 500, -51, True)
 
 # lifting the shered mission
-turn_general(-99, Kp=7)
-drive_general(400, 500, -99, True)
-drive_general(150, -500, -99, True)
-turn_general(-55)
-white_stop(200)
+turn_general(-90, Kp=7)
+
+drive_general(250, 500, -90, True)
+turn_general(-113, Kp=7)
+drive_general(150, 500, -113, True)
+drive_general(100, -500, -113, True)
+turn_general(-70)
+white_stop(100)
 right_arm_motor.run_angle(10000, -850, Stop.BRAKE, True)
 wait(1000)
 
 # place the unidentifled creature
-drive_general(50, -400, 0, False)
-# gyro restarted because the white_stop is not ansolute
+turn_general(25)
+drive_general(30, -300, 25, True)
